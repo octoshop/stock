4348c08858f4b290f8dcdcdb37540839544ca711
Dave Shoreman <dshoreman@users.noreply.github.com>
2016-06-07 09:17:25 +0100

    Add plugin skeleton

---
+++ Plugin.php
@@ -0,0 +1,20 @@
+<?php namespace Octoshop\Stock;
+
+use Backend;
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public $require = ['Octoshop.Core'];
+
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'octoshop.stock::lang.plugin.name',
+            'icon' => 'icon-shopping-cart',
+            'author' => 'Dave Shoreman',
+            'homepage' => 'http://octoshop.co/',
+            'description' => 'octoshop.stock::lang.plugin.description',
+        ];
+    }
+}
